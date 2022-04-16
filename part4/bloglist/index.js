b30db278f4a3f422933ed09eb7f3695b9d27ee5a
Kristina Maceković <macekovick@gmail.com>
2022-04-16 11:42:55 +0200

    4.1 Blog list, step1

---
+++ part4/bloglist/index.js
@@ -0,0 +1,64 @@
+/* eslint-disable no-undef */
+require('dotenv').config()
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } 
+
+    next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
