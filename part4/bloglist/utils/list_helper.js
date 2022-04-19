359fffcc715e8993fecd24ce08906c60528cfdff
Kristina Maceković <macekovick@gmail.com>
2022-04-19 07:17:59 +0200

    4.5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -7,7 +7,12 @@ const totalLikes = blogs => {
     return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
+const favoriteBlog = blogs => {
+    return blogs.reduce((prev, next) => prev.likes >= next.likes ? prev : next)
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
