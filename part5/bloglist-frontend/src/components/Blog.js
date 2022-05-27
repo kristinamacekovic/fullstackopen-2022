d247a467434bf1b5ce8689b3128788d6aac74bf7
Kristina Maceković <macekovick@gmail.com>
2022-05-27 07:28:22 +0200

    5.7* Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,46 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [detailedFlag, setDetailedFlag] = useState(false)
+
+  const toggleDetail = () => {
+    setDetailedFlag(!detailedFlag)
+  }
+
+  const detailStyle = {
+    border: "1px solid grey",
+    backgroundColor: "#E0E1DD",
+    padding: "5px",
+    margin: "10px"
+  }
+
+  const shortStyle = {
+    margin: "10px",
+    fontWeight: "bold"
+  }
+
+  if (!detailedFlag) {
+    return (
+      <div style={shortStyle}>
+        <p>
+          {blog.title} - {blog.author}
+        </p>
+        <p>
+          <button onClick={toggleDetail}>Detail</button>
+        </p>
+      </div>
+    )
+  }
+
+  return (
+    <div style={detailStyle}>
+      <p>{blog.title}</p>
+      <p>{blog.author}</p>
+      <p>{blog.url}</p>
+      <p>{blog.likes} <button>like</button></p>
+      <button onClick={toggleDetail}>Less Detail</button>
+    </div>
+  )
+}
 
 export default Blog
