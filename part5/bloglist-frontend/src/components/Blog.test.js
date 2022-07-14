8db8fb5365ab828f93ae57fb57f0493a94ab5168
Kristina Maceković <macekovick@gmail.com>
2022-07-14 20:41:26 +0200

    5.14: Blog list tests, step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('Blog renders title by default', () => {
@@ -61,4 +62,59 @@ test('Blog does not render likes by default', () => {
   const element = container.querySelector('.blogs')
 
   expect(element).not.toHaveTextContent('1')
+})
+
+test('Blog URL is shown when the details button has been clicked', async () => {
+  const blog = {
+    title: 'Testing title',
+    author: 'Testing author',
+    url: 'testing url',
+    likes: 1,
+    user: {
+      id: '12342354312'
+    }
+  }
+
+  const user = {
+    id: '12342354312'
+  }
+
+  const mockHandler = jest.fn()
+
+  const userAction = userEvent.setup()
+  const { container } = render(<Blog blog={blog} user={user} onClick={mockHandler}/>)
+
+  const button = screen.getByText('Detail')
+  const element = container.querySelector('.blogs')
+  expect(element).not.toHaveTextContent('testing url')
+  await userAction.click(button)
+  expect(element).toHaveTextContent('testing url')
+})
+
+test('Blog likes are shown when the details button has been clicked', async () => {
+  const blog = {
+    title: 'Testing title',
+    author: 'Testing author',
+    url: 'testing url',
+    likes: 999,
+    user: {
+      id: '12342354312'
+    }
+  }
+
+  const user = {
+    id: '12342354312'
+  }
+
+  const mockHandler = jest.fn()
+
+  const userAction = userEvent.setup()
+  const { container } = render(<Blog blog={blog} user={user} onClick={mockHandler}/>)
+
+  const button = screen.getByText('Detail')
+  const element = container.querySelector('.blogs')
+  expect(element).not.toHaveTextContent('999')
+  await userAction.click(button)
+  expect(element).toHaveTextContent('999')
+
 })
